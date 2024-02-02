b98d161dde64a51ee08d1f8b8d22e536305c4848
Dmitriu Ydin <da-ekb@mail.ru>
2024-02-02 15:48:30 +0500

    add HW4_Task1

--- Program.cs
+++ Program.cs
@@ -4,35 +4,23 @@
 // 5 12 16 q [STOP]
 // 3 45 342 15 [STOP]
 
-
-// Не смог решить
-// // void SearchNumber ()
-// int number;
-// bool a = false;
-// while (a)
-// {
-//     Console.WriteLine("Введите число или символ 'q' для выхода: ");
-//     string value = Console.ReadLine();   
-// }
-
-// Не моё решение
-// string input = "";
-//     int num;
-//     bool exit = true;
-//     while (exit)
-//     {
-//         input = Input();
-//         if (int.TryParse(input, out num))
-//             exit = !(num % 2 == 0);
-//         else
-//         if (input == "q")
-//             exit = false;
-//     }
-//     string Input()
-//     {
-//         Console.WriteLine("Введите число или символ 'q' для выхода");
-//         return Console.ReadLine();
-//     }
+string input = "";
+    int num;
+    bool exit = true;
+    while (exit)
+    {
+        input = Input();
+        if (int.TryParse(input, out num))
+            exit = !(num % 2 == 0);
+        else
+        if (input == "q")
+            exit = false;
+    }
+    string Input()
+    {
+        Console.WriteLine("Введите число или символ 'q' для выхода");
+        return Console.ReadLine();
+    }
 
 // Задача №2:
 // Задайте массив заполненный случайными трёхзначными числами. 
@@ -89,24 +77,24 @@
 // (первый элемент станет последним, второй – предпоследним и т.д.)
 // [1 3 5 6 7 8] => [8 7 6 5 3 1]
 
-int[] arr = { 1, 3, 5, 6, 7, 8, 4 };
-int stepEnd = 0;
-int step = 0;
-for (int i = 0; i <= (arr.Length-1) / 2; i++)
-{
-    step = arr[i];
-    arr[i] = arr[arr.Length - 1 - stepEnd];
-    arr[arr.Length - 1 - stepEnd] = step;
-    stepEnd += 1;
-}
+// int[] arr = { 1, 3, 5, 6, 7, 8, 4 };
+// int stepEnd = 0;
+// int step = 0;
+// for (int i = 0; i <= (arr.Length-1) / 2; i++)
+// {
+//     step = arr[i];
+//     arr[i] = arr[arr.Length - 1 - stepEnd];
+//     arr[arr.Length - 1 - stepEnd] = step;
+//     stepEnd += 1;
+// }
 
-void PrintArray(int[] arr)
-{
-    for (int i = 0; i < arr.Length; i++)
-    {
-        System.Console.Write(arr[i] + " ");
-    }
-    System.Console.WriteLine();
-}
+// void PrintArray(int[] arr)
+// {
+//     for (int i = 0; i < arr.Length; i++)
+//     {
+//         System.Console.Write(arr[i] + " ");
+//     }
+//     System.Console.WriteLine();
+// }
 
-PrintArray(arr);
+// PrintArray(arr);
